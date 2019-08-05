277fb1c1c746d6a299bd81cc58aec0d4f0a3ae7e
御承扬 <2923616405@qq.com>
2019-08-05 10:00:45 +0800

    添加项目文件。

---
+++ PipeServerSample/PipeServerSampleDlg.h
@@ -0,0 +1,38 @@
+﻿
+// PipeServerSampleDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CPipeServerSampleDlg 对话框
+class CPipeServerSampleDlg : public CDialogEx
+{
+// 构造
+public:
+	CPipeServerSampleDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PIPESERVERSAMPLE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	// 状态输出框
+	CString m_Log;
+	afx_msg void OnButtonConnect();
+};
